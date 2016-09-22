31bfdf33b5acd0be1f73c9330d7954dec167b657
hardyqr <Fangyu@FangyumatoMacBook-Air.local>
2016-09-22 19:37:05 +0800

    a3c

--- Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/LSTM_model_pointcloud.py
+++ Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/LSTM_model_pointcloud.py
@@ -13,14 +13,16 @@ display_step = 10
 n_input_1=6#Pointcloud data input(X,Y,Z,R,G,B)
 n_input_2=12
 #n_steps = 28 # timesteps
-n_steps=1
+n_steps=
 n_hidden = 128 # hidden layer num of features
 #n_classes = 10 # MNIST total classes (0-9 digits)
 n_classes=12#pointcloud total classes(5 furnitures,7 structral parts)
 
 
 # tf Graph input
-x = tf.placeholder("float", [batch_size, n_steps, n_input_1])
+#x = tf.placeholder("float", [batch_size, n_steps, n_input_1])
+x = tf.placeholder("float", [batch_size, None, n_input_1])
+
 # Tensorflow LSTM cell requires 2x n_hidden length (state & cell)
 istate_fw_1 = tf.placeholder("float", [batch_size, 2*n_hidden])
 istate_bw_1 = tf.placeholder("float", [batch_size, 2*n_hidden])
@@ -58,6 +60,7 @@ def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases, _batch_size, _seq_len,_
     # BiRNN requires to supply sequence_length as [batch_size, int64]
     # Note: Tensorflow 0.6.0 requires BiRNN sequence_length parameter to be set
     # For a better implementation with latest version of tensorflow, check below
+
     _seq_len = tf.fill([_batch_size], tf.constant(_seq_len, dtype=tf.int64))
 
     # input shape: (batch_size, n_steps, n_input)
@@ -80,19 +83,22 @@ def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases, _batch_size, _seq_len,_
     outputs = tf.nn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, _X,
                                             initial_state_fw=_istate_fw,
                                             initial_state_bw=_istate_bw,
-                                            sequence_length=_seq_len,)
+                                            sequence_length= _seq_len,)
 
     # Linear activation
     # Get inner loop last output
     return tf.matmul(outputs[-1], _weights['out']) + _biases['out']
 
+
 #pred = BiRNN(BiRNN(x, istate_fw_1, istate_bw_1, weights_1, biases_1, batch_size, n_steps,n_input_1),istate_fw_2, istate_bw_2, weights_2, biases_2, batch_size, n_steps,n_input_2)
+
+pred=BiRNN(x, istate_fw_1, istate_bw_1, weights_1, biases_1, batch_size,n_steps ,n_input_1)
 with tf.variable_scope('enc'):
-    layer_1_output=tf.reshape(BiRNN(x, istate_fw_1, istate_bw_1, weights_1, biases_1, batch_size, n_steps,n_input_1)
+    layer_1_output=tf.reshape(BiRNN(x, istate_fw_1, istate_bw_1, weights_1, biases_1, batch_size,n_steps ,n_input_1)
 , [128,1,12] )
 
-with tf.variable_scope('dec'):
-    pred = BiRNN(layer_1_output,istate_fw_2,istate_bw_2,weights_2,biases_2,batch_size,n_steps,n_input_2)
+#with tf.variable_scope('dec'):
+#    pred = BiRNN(layer_1_output,istate_fw_2,istate_bw_2,weights_2,biases_2,batch_size,n_steps,n_input_2)
 
 
 # Define loss and optimizer
@@ -108,7 +114,7 @@ init = tf.initialize_all_variables()
 
 # Launch the graph
 #六维特征及标签载入
-f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/beam_1.txt', 'r')
+f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/beam_1.txt', 'r')
 train_data=[]
 train_labels=[]
 for line in f:
@@ -122,7 +128,11 @@ for line in f:
     train_data.append(row)
     train_labels.append([1,0,0,0,0,0,0,0,0,0,0,0])
 f.close()
-f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/board_1.txt', 'r')
+
+
+
+
+f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/board_1.txt', 'r')
 for line in f:
     line=line.split(' ')
     row=[]
@@ -135,7 +145,7 @@ for line in f:
     train_labels.append([0,1,0,0,0,0,0,0,0,0,0,0])
 f.close()
 
-f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/board_3.txt', 'r')
+f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/board_3.txt', 'r')
 for line in f:
     line=line.split(' ')
     row=[]
@@ -148,7 +158,7 @@ for line in f:
     train_labels.append([0,1,0,0,0,0,0,0,0,0,0,0])
 f.close()
 
-f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/bookcase_1.txt', 'r')
+f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/bookcase_1.txt', 'r')
 for line in f:
     line=line.split(' ')
     row=[]
@@ -161,7 +171,7 @@ for line in f:
     train_labels.append([0,0,1,0,0,0,0,0,0,0,0,0])
 f.close()
 
-f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/ceiling_1.txt', 'r')
+f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/ceiling_1.txt', 'r')
 for line in f:
     line=line.split(' ')
     row=[]
@@ -174,7 +184,7 @@ for line in f:
     train_labels.append([0,0,0,1,0,0,0,0,0,0,0,0])
 f.close()
 
-f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/chair_1.txt', 'r')
+f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/chair_1.txt', 'r')
 for line in f:
     line=line.split(' ')
     row=[]
@@ -202,17 +212,17 @@ with tf.Session() as sess:
             batch_xs = batch_xs.reshape((batch_size, n_steps, n_input_1))
         # Fit training using batch data
             sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys,
-                                       istate_fw: np.zeros((batch_size, 2*n_hidden)),
+                                       istate_fw_1: np.zeros((batch_size, 2*n_hidden)),
                                        istate_bw: np.zeros((batch_size, 2*n_hidden))})
             if step % display_step == 0:
             # Calculate batch accuracy
                 acc = sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys,
-                                                istate_fw: np.zeros((batch_size, 2*n_hidden)),
+                                                istate_fw_1: np.zeros((batch_size, 2*n_hidden)),
                                                 istate_bw: np.zeros((batch_size, 2*n_hidden))})
             # Calculate batch loss
                 loss = sess.run(cost, feed_dict={x: batch_xs, y: batch_ys,
-                                             istate_fw: np.zeros((batch_size, 2*n_hidden)),
-                                             istate_bw: np.zeros((batch_size, 2*n_hidden))})
+                                             istate_fw_1: np.zeros((batch_size, 2*n_hidden)),
+                                             istate_bw_1: np.zeros((batch_size, 2*n_hidden))})
                 print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) +
                   ", Training Accuracy= " + "{:.5f}".format(acc))
             step += 1
@@ -224,7 +234,7 @@ with tf.Session() as sess:
 
 
     # Calculate accuracy for 128 mnist test images
-    f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/board_2.txt', 'r')
+    f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/board_2.txt', 'r')
     test_data=[]
     test_label=[]
 
@@ -245,10 +255,10 @@ with tf.Session() as sess:
     test_data = np.array(test_data[:test_len]).reshape((-1, n_steps, n_input_1))
     test_label =np.array(test_label[:test_len])
     print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label,
-                                                             istate_fw: np.zeros((test_len, 2*n_hidden)),
-                                                             istate_bw: np.zeros((test_len, 2*n_hidden))}))
+                                                             istate_fw_1: np.zeros((test_len, 2*n_hidden)),
+                                                             istate_bw_1: np.zeros((test_len, 2*n_hidden))}))
 
-    f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/codes_learning_notes/LSTM_model_pointcloud/chair_2.txt', 'r')
+    f = open('/Users/Fangyu/Documents/GitHub/Large_Scale_3D_Scene_Recognition_CVPR2017/Point_Cloud_Classification/codes_learning_notes/LSTM_model_pointcloud/chair_2.txt', 'r')
     test_data=[]
     test_label=[]
 
@@ -269,8 +279,8 @@ with tf.Session() as sess:
     test_data = np.array(test_data[:test_len]).reshape((-1, n_steps, n_input_1))
     test_label =np.array(test_label[:test_len])
     print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label,
-                                                         istate_fw: np.zeros((test_len, 2*n_hidden)),
-                                                         istate_bw: np.zeros((test_len, 2*n_hidden))}))
+                                                         istate_fw_1: np.zeros((test_len, 2*n_hidden)),
+                                                         istate_bw_1: np.zeros((test_len, 2*n_hidden))}))
 
 '''
 for line in train_data:
