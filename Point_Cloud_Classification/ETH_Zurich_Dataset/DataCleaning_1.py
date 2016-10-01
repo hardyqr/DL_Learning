10b555e474916928968122066050b925db222fed
hardyqr <Fangyu@FangyumatoMacBook-Air.local>
2016-10-01 20:57:31 +0800

    datacleaning2

---
+++ Point_Cloud_Classification/ETH_Zurich_Dataset/DataCleaning_1.py
@@ -0,0 +1,206 @@
+
+#bildstein_station1
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/bildstein_station1_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/bildstein_station1_xyz_intensity_rgb.txt', 'r')
+data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station1_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station1_xyz_rgb_20percent.txt','w')
+'''
+
+#bildstein_station3
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/bildstein_station3_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/bildstein_station3_xyz_intensity_rgb.txt', 'r')
+data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station3_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station3_xyz_rgb_20percent.txt','w')
+'''
+#bildstein_station5
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/bildstein_station5_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/bildstein_station5_xyz_intensity_rgb.txt', 'r')
+data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station5_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station5_xyz_rgb_20percent.txt','w')
+'''
+#domfountain_station1
+
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/domfountain_station1_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/domfountain_station1_xyz_intensity_rgb.txt', 'r')
+data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station1_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station1_xyz_rgb_20percent.txt','w')
+
+#domfountain_station2
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/domfountain_station2_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/domfountain_station2_xyz_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station2_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station2_xyz_rgb.txt','w')
+'''
+#domfountain_station3
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/domfountain_station3_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/domfountain_station3_xyz_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station3_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station3_xyz_rgb.txt','w')
+'''
+
+#neugasse_station1
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/neugasse_station1_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/neugasse_station1_xyz_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/neugasse_station1_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/neugasse_station1_xyz_rgb.txt','w')
+'''
+
+#sg27_station1
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/sg27_station1_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/sg27_station1_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station1_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station1_xyz_rgb.txt','w')
+'''
+#sg27_station2
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/sg27_station2_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/sg27_station2_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station2_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station2_xyz_rgb.txt','w')
+'''
+#sg27_station4
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/sg27_station4_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/sg27_station4_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station4_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station4_xyz_rgb.txt','w')
+'''
+#sg27_station5
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/sg27_station5_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/sg27_station5_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station5_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station5_xyz_rgb.txt','w')
+'''
+#sg27_station9
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/sg27_station9_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/sg27_station9_intensity_rgb.txt', 'r')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station9_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg27_station9_xyz_rgb.txt','w')
+
+
+'''
+#sg28_station4
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/sg28_station4_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/sg28_station4_intensity_rgb.txt', 'r')
+data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg28_station4_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg28_station4_xyz_rgb_20percent.txt','w')
+'''
+
+#untermaederbrunnen_station1
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/untermaederbrunnen_station1_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/untermaederbrunnen_station1_xyz_intensity_rgb.txt', 'r')
+data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/untermaederbrunnen_station1_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/untermaederbrunnen_station1_xyz_rgb_20percent.txt','w')
+
+'''
+#untermaederbrunnen_station3
+'''
+data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/untermaederbrunnen_station3_xyz_intensity_rgb.labels','r')
+data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/untermaederbrunnen_station3_xyz_intensity_rgb.txt', 'r')
+data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/untermaederbrunnen_station3_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/untermaederbrunnen_station3_xyz_rgb_percent.txt','w')
+'''
+
+
+labels=[]
+for line in data_label:
+    labels.append(line[0])
+
+count=0
+label_1=0
+label_2=0
+label_3=0
+label_4=0
+label_5=0
+label_6=0
+label_7=0
+label_8=0
+ratio_1=0
+ratio_2=0
+ratio_3=0
+ratio_4=0
+ratio_5=0
+ratio_6=0
+ratio_7=0
+ratio_8=0
+total=1
+count_1000000=0
+num=0
+
+for line in data:
+
+    row=line[:-2]
+    line=row.split(" ")
+
+    if labels[count] != '0':
+        ratio_1=float(label_1)/float(total)
+        ratio_2=float(label_2)/float(total)
+        ratio_3=float(label_3)/float(total)
+        ratio_4=float(label_4)/float(total)
+        ratio_5=float(label_5)/float(total)
+        ratio_6=float(label_6)/float(total)
+        ratio_7=float(label_7)/float(total)
+        ratio_8=float(label_8)/float(total)
+
+        if labels[count]=='1' and ratio_1<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_1=label_1+1
+        if labels[count]=='2' and ratio_2<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_2=label_2+1
+        if labels[count]=='3' and ratio_3<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_3=label_3+1
+        if labels[count]=='4' and ratio_4<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_4=label_4+1
+        if labels[count]=='5' and ratio_5<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_5=label_5+1
+        if labels[count]=='6' and ratio_6<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_6=label_6+1
+        if labels[count]=='7' and ratio_7<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_7=label_7+1
+        if labels[count]=='8' and ratio_8<0.2:
+            data_with_label_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d_20percent.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+            label_8=label_8+1
+
+
+
+        count_1000000=count_1000000+1
+        total=label_1+label_2+label_3+label_4+label_5+label_6+label_7+label_8
+    count=count+1
+
+    if count_1000000>=1000000:
+        num=num+1
+        print(str(num)+'000000')
+        print('%.3f'%ratio_1,'%.3f'%ratio_2,'%.3f'%ratio_3,'%.3f'%ratio_4,'%.3f'%ratio_5,'%.3f'%ratio_6,'%.3f'%ratio_7,'%.3f'%ratio_8)
+        count_1000000=count_1000000-1000000
+
+print(label_1,label_2,label_3,label_4,label_5,label_6,label_7,label_8)
+print(ratio_1,ratio_2,ratio_3,ratio_4,ratio_5,ratio_6,ratio_7,ratio_8)
+data.close()
+data_label.close()
+data_with_label_20percent.close()
+data_6d_20percent.close()
--- Point_Cloud_Classification/ETH_Zurich_Dataset/DataCleaning_2.py
+++ Point_Cloud_Classification/ETH_Zurich_Dataset/DataCleaning_2.py
@@ -1,32 +1,32 @@
 
 #bildstein_station1
-'''
+
 data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/bildstein_station1_xyz_intensity_rgb.labels','r')
 data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/bildstein_station1_xyz_intensity_rgb.txt', 'r')
-data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station1_xyz_intensity_rgb_label.txt','w')
-data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station1_xyz_rgb.txt','w')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station1_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station1_xyz_rgb_20percent.txt','w')
 
-'''
+
 #bildstein_station3
 '''
 data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/bildstein_station3_xyz_intensity_rgb.labels','r')
 data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/bildstein_station3_xyz_intensity_rgb.txt', 'r')
-data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station3_xyz_intensity_rgb_label.txt','w')
-data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station3_xyz_rgb.txt','w')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station3_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station3_xyz_rgb_20percent.txt','w')
 '''
 #bildstein_station5
 '''
 data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/bildstein_station5_xyz_intensity_rgb.labels','r')
 data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/bildstein_station5_xyz_intensity_rgb.txt', 'r')
-data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station5_xyz_intensity_rgb_label.txt','w')
-data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station5_xyz_rgb.txt','w')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station5_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/bildstein_station5_xyz_rgb_20percent.txt','w')
 '''
 #domfountain_station1
 '''
 data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/domfountain_station1_xyz_intensity_rgb.labels','r')
 data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/domfountain_station1_xyz_intensity_rgb.txt', 'r')
-data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station1_xyz_intensity_rgb_label.txt','w')
-data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station1_xyz_rgb.txt','w')
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station1_xyz_intensity_rgb_label_20percent.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/domfountain_station1_xyz_rgb_20percent.txt','w')
 '''
 #domfountain_station2
 '''
@@ -89,12 +89,12 @@ data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8
 
 '''
 #sg28_station4
-
+'''
 data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Ground Truth/sem8_labels_training/sg28_station4_intensity_rgb.labels','r')
 data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/sg28_station4_intensity_rgb.txt', 'r')
-data_with_label_20percent=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg28_station4_xyz_intensity_rgb_label_20percent.txt','w')
-data_6d_20percent=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg28_station4_xyz_rgb_20percent.txt','w')
-
+data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg28_station4_xyz_intensity_rgb_label.txt','w')
+data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/sg28_station4_xyz_rgb.txt','w')
+'''
 
 #untermaederbrunnen_station1
 '''
@@ -110,8 +110,8 @@ data_label= open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semant
 data=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/Training Data/untermaederbrunnen_station3_xyz_intensity_rgb.txt', 'r')
 data_with_label=open('//Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/untermaederbrunnen_station3_xyz_intensity_rgb_label.txt','w')
 data_6d=open('/Volumes/Liu\'s TOSHIBA EXT/database/ETH_Zurich_Dataset/semantic-8/untermaederbrunnen_station3_xyz_rgb.txt','w')
-
 '''
+
 
 labels=[]
 for line in data_label:
@@ -126,59 +126,106 @@ label_5=0
 label_6=0
 label_7=0
 label_8=0
-
-
+ratio_1=0
+ratio_2=0
+ratio_3=0
+ratio_4=0
+ratio_5=0
+ratio_6=0
+ratio_7=0
+ratio_8=0
+total=1
+count_1000000=0
+num=0
 
 for line in data:
+
     row=line[:-2]
     line=row.split(" ")
-    if labels[count]!='0':
-        ratio_1=label_1/total
-        ratio_2=label_2/total
-        ratio_3=label_3/total
-        ratio_4=label_4/total
-        ratio_5=label_5/total
-        ratio_6=label_6/total
-        ratio_7=label_7/total
-        ratio_8=label_8/total
-        if labels[count]=='1' and ratio_1<0.2:
-            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+
+    if labels[count] != '0':
+        ratio_1=float(label_1)/float(total)
+        ratio_2=float(label_2)/float(total)
+        ratio_3=float(label_3)/float(total)
+        ratio_4=float(label_4)/float(total)
+        ratio_5=float(label_5)/float(total)
+        ratio_6=float(label_6)/float(total)
+        ratio_7=float(label_7)/float(total)
+        ratio_8=float(label_8)/float(total)
+
+        if labels[count]=='1':
             label_1=label_1+1
-        if labels[count]=='2' and ratio_2<0.2:
-            data_with_label.write(line[0]+' '+[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='2':
             label_2=label_2+1
-        if labels[count]=='3' and ratio_3<0.2:
-            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='3':
             label_3=label_3+1
-        if labels[count]=='4' and ratio_4<0.2:
-            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='4':
             label_4=label_4+1
-        if labels[count]=='5' and ratio_5<0.2:
-            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='5':
             label_5=label_5+1
-        if labels[count]=='6' and ratio_6<0.2:
-            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='6':
             label_6=label_6+1
-        if labels[count]=='7' and ratio_7<0.2:
-            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='7':
             label_7=label_7+1
-        if labels[count]=='8' and ratio_8<0.2:
-            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
-            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='8':
             label_8=label_8+1
 
-    total=label_1+label_2+label_3+label_4+label_5+label_6+label_7+label_8
+        count_1000000=count_1000000+1
+        total=label_1+label_2+label_3+label_4+label_5+label_6+label_7+label_8
     count=count+1
+
+    if count_1000000>=1000000:
+        num=num+1
+        print(str(num)+'000000')
+        print('%.3f'%ratio_1,'%.3f'%ratio_2,'%.3f'%ratio_3,'%.3f'%ratio_4,'%.3f'%ratio_5,'%.3f'%ratio_6,'%.3f'%ratio_7,'%.3f'%ratio_8)
+        count_1000000=count_1000000-1000000
+
+print(label_1,label_2,label_3,label_4,label_5,label_6,label_7,label_8)
+#normalization
+ratio_all=1/ratio_1+1/ratio_2+1/ratio_3+1/ratio_4+1/ratio_5+1/ratio_6+1/ratio_7+1/ratio_8
+p_1=1/ratio_1/ratio_all
+p_2=1/ratio_2/ratio_all
+p_3=1/ratio_3/ratio_all
+p_4=1/ratio_4/ratio_all
+p_5=1/ratio_5/ratio_all
+p_6=1/ratio_6/ratio_all
+p_7=1/ratio_7/ratio_all
+p_8=1/ratio_8/ratio_all
+print(p_1,p_2,p_3,p_4,p_5,p_6,p_7,p_8)
+
+for line in data:
+
+    row=line[:-2]
+    line=row.split(" ")
+
+    if labels[count] != '0':
+        if labels[count]=='1' and p_1>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='2' and p_2>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='3' and p_3>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='4' and p_4>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='5' and p_5>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='6' and p_6>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='7' and p_7>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
+        if labels[count]=='8' and p_8>random.random():
+            data_with_label.write(line[0]+' '+line[1]+' '+line[2]+' '+line[3]+' '+line[4]+' '+line[5]+' '+line[6]+' '+labels[count]+'\n')
+            data_6d.write(line[0]+' '+line[1]+' '+line[2]+' '+line[4]+' '+line[5]+' '+line[6]+'\n')
 
 
 data.close()
 data_label.close()
-data_with_label_20percent.close()
-data_6d_20percent.close()
+data_with_label.close()
+data_6d.close()
