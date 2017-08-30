b587d2c83edd5d1e52e209ad8f90913ac8798978
hardyqr <liufangyu1996@gmail.com>
2017-08-31 00:58:41 +0800

    ~

--- TensorFlow-tutorial/pixel_locator.py
+++ TensorFlow-tutorial/pixel_locator.py
@@ -9,12 +9,12 @@ def pixel_locator(p, n, f ):
     p: point cloud, n: center of photo, f: focal point
     return location of the point cloud on the photo
     '''
-    point_num = len(p)
-    p = tf.Variable(p[:,0:3], tf.float32)
-    n = tf.Variable(n, tf.float32)
-    f = tf.Variable(f, tf.float32)
-
-    v = tf.subtract(f, n) # normal vector of the photo
+	point_num = len(p)
+	original_p = p
+	p = tf.Variable(p[:,0:3], tf.float32)
+	n = tf.Variable(n*point_num, tf.float32)
+	f = tf.Variable(f*point_num, tf.float32)
+	v = tf.subtract(f, n) # normal vector of the photo
     lv = tf.subtract(f, p) # line vector
     
     vpt = tf.multiply(v, lv)
@@ -26,13 +26,14 @@ def pixel_locator(p, n, f ):
         # for any point r(x,y,z) on the photo
         t = tf.divide(tf.multiply(v, tf.subtract(n, p)), vpt)
         return tf.add(p, tf.multiply(lv, t))
-data = np.loadtxt(sys.argv[1], delimiter = " ")
+
+data = np.loadtxt(sys.argv[1])
 
 print("max x: %f max y: %f max z: %f" % (max(data[:,0]),max(data[:,1]), max(data[:,2])))
 print("min x: %f min y: %f min z: %f" % (min(data[:,0]),min(data[:,1]), min(data[:,2])))
 
-photo_center = np.array(max(data[:,0]),max(data[:,1]), max(data[:,2])*1.3)
-focal_point = np.array(max(data[:,0]),max(data[:,1]), max(data[:,2])*1.5)
+photo_center = np.array([max(data[:,0]),max(data[:,1]), max(data[:,2])*1.3])
+focal_point = np.array([max(data[:,0]),max(data[:,1]), max(data[:,2])*1.5])
 
 
 print(data.shape)
