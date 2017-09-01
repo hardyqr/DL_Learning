b89a8fe96d173ab12ae063e5c9ed969ae3c18429
hardyqr <liufangyu1996@gmail.com>
2017-09-02 05:01:26 +0800

    ~

--- TensorFlow-tutorial/pixel_locator.py
+++ TensorFlow-tutorial/pixel_locator.py
@@ -1,7 +1,10 @@
-
+# Freddy @Terminal 1, Capital Airport, Beijing
+# Sep 2, 2017
 import tensorflow as tf
 import numpy as np
 from os import sys
+import time
+import cv2
 
 '''
 def pixel_locator():
@@ -27,54 +30,73 @@ def pixel_locator():
         return tf.add(p, tf.multiply(lv,t[0])), p,n,f
 '''
 
-
 def pixel_locator():
     x = tf.placeholder(tf.float32,[None,3])
     p = tf.placeholder(tf.float32,[None,3])
     v = tf.placeholder(tf.float32,[None,3])
     f = tf.placeholder(tf.float32,[None,1])
     divider = tf.reduce_sum( (p - x) * v , axis=0, keep_dims=True) + 1 
-    ret = f * v - f * f * f * (p + f * v - x) / divider
+    result = f * v - f * f * f * (p + f * v - x) / divider
 
-    return ret, x, p, v,f
+    return result, x, p, v,f
 
-print("load data...")
-original_data = np.loadtxt(sys.argv[1])
-print("load complete")
-print("max x: %f max y: %f max z: %f" % (max(original_data[:,0]),max(original_data[:,1]), max(original_data[:,2])))
-print("min x: %f min y: %f min z: %f" % (min(original_data[:,0]),min(original_data[:,1]), min(original_data[:,2])))
 
-photo_center = np.array([max(original_data[:,0])*1.2,max(original_data[:,1])*1.2, max(original_data[:,2])*1.3])
-focal_dir = np.array( [0 , 0 , max(original_data[:,2])*1.5 ] )
-#focal_dir = np.array([max(original_data[:,0])*1.2,max(original_data[:,1])*1.2, max(original_data[:,2])*1.5])
-focal_distance = (max(original_data[:,2]) - min(original_data[:,2]))*0.3
+def timeit(func, *args, **kwargs):
+    s = time.time()
+    func(*args, **kwargs)
+    e = time.time()
+    print("USE <\033[1;32m%.2f\033[0m> s" % (e - s))
 
-#print(data.shape)
-data,x,p,v,f = pixel_locator()
-print(data.shape)
-
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-    sess.run(init)
-    point_num = len(original_data)
-    photo_center = np.transpose(np.reshape(np.repeat(photo_center,point_num),(3,point_num)))
-    focal_dir = np.transpose(np.reshape(np.repeat(focal_dir,point_num),(3,point_num)))
-    focal_distance = np.transpose(np.reshape(np.repeat(focal_distance,point_num),(1,point_num)))
-    
-    print("photo_center.shape:", photo_center.shape)
-    print("focal_dir.shape: ",focal_dir.shape)
-    print("focal_distance.shape: ",focal_distance.shape)
-    
-    print("to run...")
-    data = sess.run(data,feed_dict={
-        x: original_data[:,:3] ,
-        p: photo_center ,
-        v: focal_dir,
-        f: focal_distance }) # any data returned by sess.run is numpy.array
+def data_loader(address):
+    print("load data...")
+    original_data = np.loadtxt(address)
+    print("load complete")
+    print("------data stats------")
     print("max x: %f max y: %f max z: %f" % (max(original_data[:,0]),max(original_data[:,1]), max(original_data[:,2])))
-    print(max(data[:,0]), max(data[:,1]),max(data[:,2]))
-    #print(data)
-    original_data[:,:3] = data
-    #print(data.shape)
-    np.savetxt(sys.argv[2], original_data, fmt = "%.3f %.3f %.3f %i %i %i %i")
+    print("min x: %f min y: %f min z: %f" % (min(original_data[:,0]),min(original_data[:,1]), min(original_data[:,2])))
+    return original_data
 
+
+def parameter_setting():
+    photo_center = np.array([max(original_data[:,0])*1.2,max(original_data[:,1])*1.2, max(original_data[:,2])*1.3])
+    focal_dir = np.array( [0 , 0 , max(original_data[:,2])*1.5 ] )
+    focal_distance = (max(original_data[:,2]) - min(original_data[:,2]))*0.3
+    return photo_center, focal_dir, focal_distance
+
+def main():
+    original_data = data_loader(sys.argv[1])
+    data,x,p,v,f = pixel_locator()
+
+    photo_center, focal_dir, focal_distance = parameter_setting()
+    
+    init = tf.global_variables_initializer()
+    
+    with tf.Session() as sess:
+        sess.run(init)
+        point_num = len(original_data)
+        photo_center = np.transpose(np.reshape(np.repeat(photo_center,point_num),(3,point_num)))
+        focal_dir = np.transpose(np.reshape(np.repeat(focal_dir,point_num),(3,point_num)))
+        focal_distance = np.transpose(np.reshape(np.repeat(focal_distance,point_num),(1,point_num)))
+        
+        print("photo_center.shape:", photo_center.shape)
+        print("focal_dir.shape: ",focal_dir.shape)
+        print("focal_distance.shape: ",focal_distance.shape)
+        
+        print("to run...")
+        
+        data = sess.run(data,feed_dict={
+            x: original_data[:,:3] ,
+            p: photo_center ,
+            v: focal_dir,
+            f: focal_distance }) # any data returned by sess.run is numpy.array
+        print("max x: %f max y: %f max z: %f" % (max(original_data[:,0]),max(original_data[:,1]), max(original_data[:,2])))
+        print(max(data[:,0]), max(data[:,1]),max(data[:,2]))
+        #print(data)
+        original_data[:,:3] = data
+        #print(data.shape)
+        np.savetxt(sys.argv[2], original_data, fmt = "%.3f %.3f %.3f %i %i %i %i")
+
+
+
+if __name__ == "__main__":
+    timeit(main,*args)
